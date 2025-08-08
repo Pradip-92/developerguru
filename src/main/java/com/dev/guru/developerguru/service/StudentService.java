158dd9c58accf39d4853933dce4236440803786c
Pradip-92 <pradipshinde013@gmail.com>
2025-08-09 01:49:48 +0530

    To handle  exception try-catch block added into getStudentByName() method

--- src/main/java/com/dev/guru/developerguru/service/StudentService.java
+++ src/main/java/com/dev/guru/developerguru/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.dev.guru.developerguru.service;
 
 import com.dev.guru.developerguru.entity.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private Logger log = LogManager.getLogger(StudentService.class);
+
     public Student getStudent() {
         Student s = Student.builder().
                 sid(101).name("Ganesh").age(22).gender("Male").build();
@@ -15,8 +19,13 @@ public class StudentService {
     }
 
     public Student getStudentByName(String name) {
-        Student s = Student.builder().
-                sid(101).name("Ganesh").age(22).gender("Male").build();
+        Student s = null;
+        try {
+            Student.builder().
+                    sid(101).name("Ganesh").age(22).gender("Male").build();
+        } catch (Exception e) {
+            log.error("Exception :- {}", e.getMessage());
+        }
         return s;
     }
 
